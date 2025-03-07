f70132a6c015e8d5047c69ed2f8e07eb80199791
Kittisak Keawpichai <6710110030@psu.ac.th>
2025-03-07 14:19:36 +0700

    add process

--- fibonacci.py
+++ fibonacci.py
@@ -1,20 +1,31 @@
 import multiprocessing
+import concurrent.futures
 import time
 
 def fibonacci(n):
-    number =[0,1]
-    for i in range(2,n):
-        number.append(number[i-1]+number[i-2])
-    return number
+    num = [0, 1]
+    for i in range(2, n):
+        num.append(num[i-1] + num[i-2])
+    return num
 
 def multi(n):
     with multiprocessing.Pool() as pool:
-        pool.map(fibonacci, [n])
+        pool.map(fibonacci, n) 
+
+def synchronous(n):
+    num = []
+    for i in n:
+        num.append(fibonacci(i))
+    return num
+
+def threaded(n):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.map(fibonacci, n)
 
 if __name__ == "__main__":
-    n = 10**5
+    n = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
     print("Starting calculation")
-    start = time.time()
-    multi(n)
-    duration = time.time() - start
-    print(f"Duration {duration} seconds")
+    start_multi = time.time()
+    multi_results = threaded(n)
+    duration_multi = time.time() - start_multi
+    print(f"Multiprocessing Duration: {duration_multi} seconds")
