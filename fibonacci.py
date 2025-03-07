f529669a0c25b330eae21cf128ed170ae2738ac2
Kittisak Keawpichai <6710110030@psu.ac.th>
2025-03-07 14:28:05 +0700

    asncio

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,3 @@
-import multiprocessing
-import concurrent.futures
 import time
 
 def fibonacci(n):
@@ -8,24 +6,16 @@ def fibonacci(n):
         num.append(num[i-1] + num[i-2])
     return num
 
-def multi(n):
-    with multiprocessing.Pool() as pool:
-        pool.map(fibonacci, n) 
-
 def synchronous(n):
     num = []
     for i in n:
         num.append(fibonacci(i))
     return num
 
-def threaded(n):
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        executor.map(fibonacci, n)
-
 if __name__ == "__main__":
     n = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
     print("Starting calculation")
     start_multi = time.time()
-    multi_results = threaded(n)
+    multi_results = synchronous(n)
     duration_multi = time.time() - start_multi
-    print(f"Multiprocessing Duration: {duration_multi} seconds")
+    print(f"Duration: {duration_multi} seconds")
